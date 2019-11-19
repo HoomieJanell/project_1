6822cba46f798a77e750059d8938740ef6c7ddd2
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-18 18:33:36 -0600

    commiting 11/18 no idea what changes are made on this commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+$(document).ready(function(){
 
+    var lat;
+    var lng;
+    getLocation();
+    function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+      
+      function showPosition(position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        console.log(lat);
+        console.log(lng);
+
+      }
+      var queryURL = "https://api.songkick.com/api/3.0/search/locations.json?location=geo:"+lat+","+lng+"&apikey="+apiKey
+      var apiKey = "eWIxuDJ59JLKMebO";
+  
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+
+});
