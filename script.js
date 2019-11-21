ef980c4757cabb645c86a3fc43ad55fb3da83197
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-20 18:58:25 -0600

    merging 5 day with master

--- script.js
+++ script.js
@@ -1,57 +1,29 @@
-$(document).ready(function(){
-
-    var lat;
-    var lng;
-    // getLocation();
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //       navigator.geolocation.getCurrentPosition(showPosition);
-    //     } else { 
-    //       x.innerHTML = "Geolocation is not supported by this browser.";
-    //     }
-    //   }
-      
-      // function showPosition(position) {
-      //   lat = position.coords.latitude;
-      //   lng = position.coords.longitude;
-      //   console.log(lat);
-      //   console.log(lng);
-
-      // }
-      var apiKey = "eWIxuDJ59JLKMebO";
-      var areaID;
-
-      // var queryURL = "https://api.songkick.com/api/3.0/search/locations.json?location=geo:"+lat+","+lng+"&apikey="+apiKey;
-      var queryURL = "https://api.songkick.com/api/3.0/search/locations.json?location=clientip"+"&apikey="+apiKey+"&per_page=1&page=1";
+$(document).ready(function () {
+  var apiKey = "eWIxuDJ59JLKMebO";
+  var areaID;
+  var queryURL = "https://api.songkick.com/api/3.0/search/locations.json?location=clientip" + "&apikey=" + apiKey + "&per_page=1&page=1";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    areaID = response.resultsPage.results.location[0].metroArea.id;
+    // console.log(response);
+    // console.log(areaID);
+    queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + areaID + "/calendar.json?apikey=" + apiKey + "&per_page=5";
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        areaID = response.resultsPage.results.location[0].metroArea.id;
-        console.log(areaID);
-        queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + areaID + "/calendar.json?apikey=" + apiKey+"&per_page=5";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-      }).then(function(response){
-          console.log(response);
-          console.log(areaID);
-          for(i=0;i < response.resultsPage.results.event.length; i++){
-            var date = moment((response.resultsPage.results.event[i].start.date).replace(/-/g, '')).format("MMM Do YY");
-            $(".title"+i).text(response.resultsPage.results.event[i].performance[0].displayName);
-            $(".sub"+i).text(response.resultsPage.results.event[i].venue.displayName);
-            $(".date"+i).text(date);
-
-            console.log(date);
-
-          }
-      })
-    
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      // console.log(response);
+      // console.log(areaID);
+      for (i = 0; i < response.resultsPage.results.event.length; i++) {
+        var date = moment((response.resultsPage.results.event[i].start.date).replace(/-/g, '')).format("MMM Do YY");
+        var title = response.resultsPage.results.event[i].performance[0].displayName;
+        var subtitle = response.resultsPage.results.event[i].venue.displayName;
+        $(".title" + i).text(title);
+        $(".sub" + i).text(subtitle);
+        $(".date" + i).text(date);
+      }
     })
-    // queryURL = "https://api.songkick.com/api/3.0/metro_areas/9179/calendar.json?apikey=eWIxuDJ59JLKMebO";
-  //   $.getJSON("https://api.ipify.org/?format=json", function(e) {
-  //     console.log(e.ip);
-  //     var ip = e.ip;
-  // });
+  })
 });
