dde228c96742d433333f72c352b302612d544e15
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-22 21:12:18 -0600

    search bar is working

--- script.js
+++ script.js
@@ -29,25 +29,37 @@ $(document).ready(function () {
   // })
   //End 5 day
   var urlParams = new URLSearchParams(window.location.search);
-  console.log(urlParams.get('key'));
-  searchResults();
+  var searchKey = urlParams.get('key');
+  console.log(searchKey);
+  searchResults(searchKey);
 function searchResults(keyword){
   var apikey = "ngKYGjzcV2SX5MIxtwyrTztf3GCREITJ"
   $.ajax({
     method:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword="+ keyword + "&city=a  ustin"+"&apikey=" + apikey,
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword="+ keyword + "&city=austin"+"&apikey=" + apikey,
     success: function(response) {
                 console.log(response);
                 console.log((response._embedded.events).length);
                 // Parse the response.
                 // Do other things.
-                var resultsTitle = $("<h1>").text("Results for: "+ $("#searchbar").val());
+                var resultsTitle = $("<h1>").text("Results for: "+searchKey);
                 resultsTitle.attr("id", "resultsTitle");
                 $("#results").append(resultsTitle);
-                for (i = 0; i < (response._embedded.events).length; i++){
-                  var result = $("<h1>").text(response._embedded.events[i].name);
-                  result.attr("id", "resultRow");
+                for (i = 0; i < 8; i++){
+                  var date = $("<div>").text(moment((response._embedded.events[i].dates.start.localDate).replace(/-/g, '')).format("MMM Do YYYY"));
+                  date.addClass("resultRow")
+                  date.attr("style", "width: 300px")
+                  var event = $("<div>").text(response._embedded.events[i].name);
+                  event.addClass("resultRow")
+                  var result = $("<div>");
+                  result.addClass("rows");
+                  result.attr({
+                    id:"resultRow"+i,
+                    onclick: "location.href="+"'"+response._embedded.events[i].url+"'",
+                    style: "background-image: url('"+response._embedded.events[i].images[9].url
+                });
                   $("#results").append(result);
+                  $("#resultRow"+i).append(date, event);
                 }
              },
     error: function(xhr, status, err) {
@@ -58,23 +70,27 @@ function searchResults(keyword){
              }
   });
 }
-$("#searchbutton").on("click", function(event){
+$("#searchbutton").on("click", function(){
   console.log("cndfsdv");
   console.log(window.location.href)
 
   if (window.location.href === "file:///C:/Users/jpellow/bootcamp/project_1/results.html"){
     console.log("if")
-
     var searchTerm = $("#searchbar").val();
-    searchResults(searchTerm);
+    window.location.href = "file:///C:/Users/jpellow/bootcamp/project_1/results.html?key="+searchTerm;
+    var urlParams = new URLSearchParams(window.location.search);
+    searchResults(urlParams.get('key'));
+
   }
   else{
     console.log("else")
     var searchTerm = $("#searchbar").val();
-    event.preventDefault();
     window.location.href = "file:///C:/Users/jpellow/bootcamp/project_1/results.html?key="+searchTerm;
+    var urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get('key'));
+  
     console.log("dfvsdvsdfv");
-    searchResults(searchTerm);
+    searchResults(urlParams.get('key'));
   }
 
 })
